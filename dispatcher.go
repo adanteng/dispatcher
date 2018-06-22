fac3c09807ba4cebdbdca15f01a99934521ea0ac
lihao <haoli2345@sf-express.com>
2018-06-22 14:47:54 +0800

    add some new api to include more flexibility

--- dispatcher.go
+++ dispatcher.go
@@ -2,25 +2,111 @@ package dispatcher
 
 import (
 	"context"
+	"fmt"
+	"sync"
 )
 
-type Dispatcher struct {
-	inputQueue     chan interface{}
-	workerPool     chan chan interface{}
-	createExecuter CreateExecuter
+type dispOptions struct {
+	// buffer length, but when consumer is so slow this will change nothing
+	queueSize int64
+	// make the consume faster, according the cpu load
+	workerCount int64
+
+	executerBuilder Builder
 }
 
-func NewDispatcher(queueSize, workerCount int, createExecuter CreateExecuter) *Dispatcher {
-	return &Dispatcher{
-		inputQueue:     make(chan interface{}, queueSize),
-		workerPool:     make(chan chan interface{}, workerCount),
-		createExecuter: createExecuter,
+type Builder interface {
+	Build() (Executer, error)
+}
+
+type dispOptionsFunc func(*dispOptions)
+
+func WithQueueSize(s int64) dispOptionsFunc {
+	return func(o *dispOptions) {
+		o.queueSize = s
 	}
 }
 
+func WithWorkerCount(s int64) dispOptionsFunc {
+	return func(o *dispOptions) {
+		o.workerCount = s
+	}
+}
+
+func WithExecuterBuilder(b Builder) dispOptionsFunc {
+	return func(o *dispOptions) {
+		o.executerBuilder = b
+	}
+}
+
+var defaultDispOptions = dispOptions{
+	queueSize:   50,
+	workerCount: 3,
+}
+
+type Dispatcher struct {
+	inputQueue chan interface{}
+	workerPool chan chan interface{}
+
+	// DEPRECATED: Not recommended to use this way.
+	createExecuter CreateExecuter
+
+	// Incase app SetExecuter to nil, add mu.
+	once     sync.Once
+	executer Executer
+}
+
+func NewDispatcher(queueSize, workerCount int, createExecuter CreateExecuter) *Dispatcher {
+	d := &Dispatcher{
+		inputQueue: make(chan interface{}, queueSize),
+		workerPool: make(chan chan interface{}, workerCount),
+	}
+
+	if createExecuter == nil {
+		fmt.Println("createExecuter should not be nil")
+		return d
+	}
+	d.executer = createExecuter()
+
+	return d
+}
+
+// MUST: Use SetExecuter after call this func
+func New(opt ...dispOptionsFunc) (*Dispatcher, error) {
+	opts := defaultDispOptions
+	for _, o := range opt {
+		o(&opts)
+	}
+
+	d := &Dispatcher{
+		inputQueue: make(chan interface{}, opts.queueSize),
+		workerPool: make(chan chan interface{}, opts.workerCount),
+	}
+
+	// May use SetExecuter too.
+	if opts.executerBuilder != nil {
+		executer, err := opts.executerBuilder.Build()
+		if err != nil {
+			return nil, err
+		}
+		d.executer = executer
+	}
+
+	// start worker according to workerPool directly,
+	// and start to dispatch workerPool if msg come in.
+	d.startWorker()
+
+	return d, nil
+}
+
+// DEPRECATED: Use NewDispatcher(opt ...dispOptionsFunc), not necessary anymore.
 func (d *Dispatcher) StartWorkers() {
+	d.startWorker()
+}
+
+func (d *Dispatcher) startWorker() {
 	for i := 0; i < cap(d.workerPool); i++ {
-		worker := newWorker(d.workerPool, d.createExecuter())
+		worker := newWorker(d.workerPool, d)
 		worker.start()
 	}
 
@@ -30,6 +116,10 @@ func (d *Dispatcher) StartWorkers() {
 			idleWorker <- m
 		}
 	}()
+}
+
+func (d *Dispatcher) SetExecuter(e Executer) {
+	d.once.Do(func() { d.executer = e })
 }
 
 // DEPRECATED: Use PublishM instead.
@@ -49,16 +139,17 @@ func (d *Dispatcher) PublishM(c context.Context, dat interface{}) {
 }
 
 type worker struct {
+	// keep the ptr to Dispatcher
+	dptr      *Dispatcher
 	queuePool chan chan interface{}
 	queue     chan interface{}
-	executer  Executer
 }
 
-func newWorker(pool chan chan interface{}, executer Executer) *worker {
+func newWorker(pool chan chan interface{}, d *Dispatcher) *worker {
 	return &worker{
 		queuePool: pool,
 		queue:     make(chan interface{}),
-		executer:  executer,
+		dptr:      d,
 	}
 }
 
@@ -68,7 +159,11 @@ func (w *worker) start() {
 			w.queuePool <- w.queue
 			select {
 			case m := <-w.queue:
-				w.executer.Do(m)
+				if w.dptr.executer != nil {
+					w.dptr.executer.Do(m)
+				} else {
+					fmt.Println("nil executer")
+				}
 			}
 		}
 	}()
--- dispatcher_test.go
+++ dispatcher_test.go
@@ -1,14 +1,13 @@
 package dispatcher
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"testing"
-	"time"
-
-	"golang.org/x/net/context"
 )
 
+// For
 func Test_dispatcher(t *testing.T) {
 	dpr := NewDispatcher(1, 2, func() Executer {
 		return &TestExecuter{}
@@ -31,7 +30,6 @@ func Test_dispatcher(t *testing.T) {
 	}
 
 	wg.Wait()
-	time.Sleep(1 * time.Second)
 }
 
 func Test_PublishM(t *testing.T) {
@@ -58,7 +56,41 @@ func Test_PublishM(t *testing.T) {
 	}
 
 	wg.Wait()
-	time.Sleep(1 * time.Second)
+}
+
+func Test_NewDispatcher(t *testing.T) {
+	dpr, _ := New(
+		WithQueueSize(1),
+		WithWorkerCount(2),
+		WithExecuterBuilder(&testBuilder{}),
+	)
+
+	dpr.Publish("test publish")
+	dpr.PublishM(context.Background(), "test publishm")
+
+	var wg sync.WaitGroup
+
+	ctx := context.Background()
+	f := func(no int) {
+		for i := 1; i <= 5; i++ {
+			cctx := context.WithValue(ctx, i, i)
+			dpr.PublishM(cctx, fmt.Sprintf("with ctx2 %d-%d", no, i))
+		}
+		wg.Done()
+	}
+
+	for j := 1; j <= 5; j++ {
+		wg.Add(1)
+		go f(j)
+	}
+
+	wg.Wait()
+}
+
+type testBuilder struct{}
+
+func (*testBuilder) Build() (Executer, error) {
+	return &TestExecuter{}, nil
 }
 
 type TestExecuter struct {
